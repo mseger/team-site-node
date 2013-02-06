4c2a03df37192eda0a11194c2fa5b1e249720205
Margaret-Ann Seger <margaret-ann@Margaret-Anns-MacBook-Air.local>
2013-02-06 10:26:39 -0500

    Added homepage route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , homepage = require('./routes/homepage')
   , http = require('http')
   , path = require('path');
 
@@ -27,7 +28,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', homepage.display_welcome);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
---
+++ routes/homepage.js
@@ -0,0 +1,6 @@
+
+// homepage display & functionality
+
+exports.display_welcome = function(req, res){
+  res.render('homepage', {title: "Lifegraph Labs"});
+};
