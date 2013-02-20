71450fd361e5ff1a847c8624a07eae003098707c
Shane Moon <smoon@shanemoon.(none)>
2013-02-20 11:06:42 -0500

    changed the video, added google analytics

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -11,5 +11,16 @@
 // Twitter Widget
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
+// Google Analytics
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38659148-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 // Image Slider Thumbnail
 //imageSlider.thumbnailPreview(function (thumbIndex) { return "<img src='images/thumb" + (thumbIndex + 1) + ".jpg' style='width:70px;height:44px;' />"; });
