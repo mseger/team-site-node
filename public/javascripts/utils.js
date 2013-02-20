6343e5ed0c97888b1e81e58df0153720c1819b93
Shane Moon <smoon@shanemoon.(none)>
2013-02-20 11:30:47 -0500

    placeholder contact

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -23,4 +23,4 @@
   })();
 
 // Image Slider Thumbnail
-//imageSlider.thumbnailPreview(function (thumbIndex) { return "<img src='images/thumb" + (thumbIndex + 1) + ".jpg' style='width:70px;height:44px;' />"; });
+// imageSlider.thumbnailPreview(function (thumbIndex) { return "<img src='images/thumb" + (thumbIndex + 1) + ".jpg' style='width:70px;height:44px;' />"; });
